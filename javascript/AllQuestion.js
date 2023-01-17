b845385c252c5750e87b557cb8821a4eeb9dbb4f
prajwal zingare <prajwalzingre@gmail.com>
2023-01-17 19:55:39 +0530

    question added

--- javascript/AllQuestion.js
+++ javascript/AllQuestion.js
@@ -4,21 +4,21 @@ console.log(is_string('w3resource'));
 true
 console.log(is_string([1, 2, 4, 0]));
 false */
-console.log("data");
-let inpt = document.getElementById("inpt");
-let btn = document.getElementById("btn");
-btn.addEventListener("click", myFunction);
+// console.log("data");
+// let inpt = document.getElementById("inpt");
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", myFunction);
 
-function myFunction() {
-  console.log("run");
-  let a = inpt.value;
-  console.log(a);
-  if (isNaN(a)) {
-    console.log("is string");
-  } else {
-    console.log("not a string");
-  }
-}
+// function myFunction() {
+//   console.log("run");
+//   let a = inpt.value;
+//   console.log(a);
+//   if (isNaN(a)) {
+//     console.log("is string");
+//   } else {
+//     console.log("not a string");
+//   }
+// }
 
 // 2. Remove Duplicate characters from array of element and find the count of an elements using set object
 var arr = [55, 44, 55, 67, 67, 67, 67, 8, 8, 8, 8, 8, 65, 1, 2, 3, 3, 34, 5];
@@ -35,19 +35,43 @@ function withoutReverse() {
 console.log(withoutReverse());
 output = "yrtnuoc ym si aidnI";
 //3.  Swapping of 2 numbers with third variable
-let a = 10;
-let b = 20;
-let c;
-c = a;
-a = b;
-b = c;
-console.log(a, b, c);
+// let a = 10;
+// let b = 20;
+// let c;
+// c = a;
+// a = b;
+// b = c;
+// console.log(a, b, c);
 
 //4. To find the index of an element from an array
-const letters = ["a", "b", "c"];
-const index = letters.indexOf("b");
-console.log(index); // `1`
+// const letters = ["a", "b", "c"];
+// const index = letters.indexOf("b");
+// console.log(index); // `1`
 
 // const set = new Set([1,1,2,2,3,4,5])
 // console.log(set) //{1,2,3,4,5}
 //Explaination : It will remove duplicate values.
+
+/* question-5 Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8. */
+/* 
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let para = document.getElementById("para");
+btn.addEventListener("click", insertDash);
+
+function insertDash() {
+  let str = input.value;
+  console.log(str[1]);
+  let result = [str[0]];
+  console.log(result);
+  for (var x = 1; x < str.length; x++) {
+    if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
+      result.push("-", str[x]);
+    } else {
+      result.push(str[x]);
+    }
+    para.innerText = result.join("");
+  }
+  console.log(result);
+}
+ */
