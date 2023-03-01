80729a70009ef6e903743b4f5226741fa6efa373
prajwal zingare <prajwalzingre@gmail.com>
2023-03-01 20:05:29 +0530

    update:[code updated]

--- Basic_question.js
+++ Basic_question.js
@@ -1,4 +1,4 @@
-console.log(true); //true
+/* console.log(true); //true
 console.log(typeof true); // boolean
 console.log(typeof true === "boolean"); // true
 
@@ -31,9 +31,10 @@ emptyString ? console.log(true) : console.log(false); // false
 Boolean({}) ? console.log(true) : console.log(false); // true
 Boolean("") ? console.log(true) : console.log(false); // false
 
+ */
 /* 
 The falsy values in JavaScript are false, 0, -0, 0n, null, undefined, NaN, and the empty string ""; everything else is a truthy value. All objects are truthy, including empty objects {} and arrays []. */
-
+/* 
 // falsy values: false, 0, -0, 0n, null, undefined, NaN, and the empty string ""
 console.log(Boolean(false)); // false
 console.log(Boolean(0)); // false
@@ -68,7 +69,7 @@ console.log("" ? "true" : "false"); // false
 console.log(0 ? "true" : "false"); // false
 console.log(NaN ? "true" : "false"); // false
 console.log(undefined ? "true" : "false"); // false
-console.log(null ? "true" : "false"); // false
+console.log(null ? "true" : "false"); // false */
 
 //question
 // let a = [];
